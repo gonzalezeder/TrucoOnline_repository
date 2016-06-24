543c6b634f83f7e9389a5438ee02de8654aeef1c
gonzalezeder <gonzalez.eder@gmail.com>
2016-06-24 11:48:05 -0300

    Cambios en DTO's

--- TrucoOnline_repository/src/dtos/EstadoDTO.java
+++ TrucoOnline_repository/src/dtos/EstadoDTO.java
@@ -1,5 +1,26 @@
 package dtos;
 
 public class EstadoDTO {
-
+	private int id;
+	private String estado;
+	
+	public EstadoDTO(int id, String estado){
+		this.id=id;
+		this.estado=estado;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
 }
--- TrucoOnline_repository/src/dtos/ParejaDTO.java
+++ TrucoOnline_repository/src/dtos/ParejaDTO.java
@@ -1,5 +1,35 @@
 package dtos;
 
-public class ParejaDTO {
 
+
+public class ParejaDTO {
+	private int idPareja;
+	private JugadorDTO jugador1;
+	private JugadorDTO jugador2;
+	
+	
+	public ParejaDTO(JugadorDTO jugador1, JugadorDTO jugador2) {
+		this.jugador1 = jugador1;
+		this.jugador2 = jugador2;
+	}
+	public int getIdPareja() {
+		return idPareja;
+	}
+	public void setIdPareja(int idPareja) {
+		this.idPareja = idPareja;
+	}
+	public JugadorDTO getJugador1() {
+		return jugador1;
+	}
+	public void setJugador1(JugadorDTO jugador1) {
+		this.jugador1 = jugador1;
+	}
+	public JugadorDTO getJugador2() {
+		return jugador2;
+	}
+	public void setJugador2(JugadorDTO jugador2) {
+		this.jugador2 = jugador2;
+	}
+	
+	
 }
